c2feea461dc5bc894a7f794ba589cf6d1ae98164
Daniel Vieira <danielcei@gmail.com>
2025-04-13 17:41:10 -0300

    Agendamento

--- database/migrations/2025_04_13_045312_create_unavailable_dates_table.php
+++ database/migrations/2025_04_13_045312_create_unavailable_dates_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('unavailable_dates', function (Blueprint $table) {
             $table->id();
-            $table->softDeletes();
             $table->date('date')->unique();
             $table->string('reason')->nullable();
             $table->softDeletes();
